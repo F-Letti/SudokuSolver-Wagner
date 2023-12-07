daebe5588369ea04c0d65f3334aa2fa05cf954fe
Florian Lettenbichler <lettenbichler.florian@gmail.com>
2023-12-07 14:38:02 +0100

    fix conflicts

--- SudokuSolver/SudokuBoard.cs
+++ SudokuSolver/SudokuBoard.cs
@@ -103,5 +103,24 @@ namespace SudokuSolver {
       }
       return status;
     }
+
+    
+
+    public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0;i < size;i++)
+            {
+                for (int j = 0;j < size;j++)
+                {
+                    sb.Append($"{board[i, j]}" + " ");
+                }
+
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
   }
 }
